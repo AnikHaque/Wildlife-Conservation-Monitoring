9b570729463a3e416d71748b646ae0effd832bea
AnikHaque <ekramul15-811@diu.edu.bd>
2025-05-26 04:25:03 +0600

    species table created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -12,3 +12,11 @@ INSERT INTO rangers (name, region) VALUES
 ('Alice Green', 'Northern Hills'), 
 ('Bob White', 'River Delta'), 
 ('Carol King', 'Mountain Range');
+
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(50) NOT NULL, 
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL, 
+    conservation_status VARCHAR(20) NOT NULL
+)
